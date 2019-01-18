e1db441b4b3da56e7a8bba3c19a5d38667b92c26
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-18 08:37:40 -0800

    palindrome solution

--- stack.js
+++ stack.js
@@ -2,8 +2,8 @@
 
 class _Node{
   constructor(data, next){
-    this.data = null;
-    this.next = null;                
+    this.data = data;
+    this.next = next;                
   }
 }
 
@@ -16,16 +16,17 @@ class Stack {
   pop(){
     let node = this.top;
     this.top = this.top.next; 
-    return node;
+    return node.data;
   }
 
   push(item){
     if(this.top===null){
       this.top = new _Node(item, null);
+      return;
     }
 
-    let current = this.top;
-    this.top = new _Node(item, current);
+    let node = new _Node(item, this.top);
+    this.top = node;
   }
    
 }
@@ -40,10 +41,39 @@ function peek(stack){
 function display(stack){
   let current = stack.top;
   if (current === null){
-    console.log('You have no stack to display')
+    console.log('You have no stack to display');
   }
   while(current !== null){
     console.log(current.value);
     current= current.next;
   }
 }
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  
+  //first create a stack by pushing in each letter from the string
+  let stack = new Stack();
+  for(let i = 0; i < s.length; i++){
+    stack.push(s[i]);
+  }
+
+  //then pop off each item in the stack and check it against it letter in the string
+  let bool;
+  for(let i = 0; i < s.length; i++){
+    let top = stack.pop();
+    bool = top !== s[i] ? false : true;
+    if(bool===false){
+      return false;
+    }
+  }
+  return bool;
+}
+
+// "mom" m --> o --> m
+
+// true, true, true
+console.log(is_palindrome('dad'));
+console.log(is_palindrome('A man, a plan, a canal: Panama'));
+console.log(is_palindrome('1001'));
+console.log(is_palindrome('Tauhida'));
